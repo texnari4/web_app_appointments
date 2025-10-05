34b776412e95a434dee57800bca353d6629e6ecd
texnari4 <39917166+texnari4@users.noreply.github.com>
2025-10-05 20:21:49 +0300

    run

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo ">>> Building and starting the application containers..."
-docker-compose up --build -d
-echo ""
-echo ">>> Application is starting!"
-echo ">>> Backend API will be available at http://localhost:5000"
-echo ">>> To see logs, run: docker-compose logs -f"
-echo ">>> To stop the application, run: docker-compose down"
